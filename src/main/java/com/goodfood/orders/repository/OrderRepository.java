ecd81e6a787e6ed4ca9e0b1556535ef0cdaf3973
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-25 20:27:14 -0300

    feat: add custom queries for order management
    - Add searchForIdWithItems() query to fetch an order with its items by ID using LEFT JOIN FETCH.
    - Add updateStatus() query to update the status of a specific order with @Modifying and @Transactional annotations.

--- src/main/java/com/goodfood/orders/repository/OrderRepository.java
+++ src/main/java/com/goodfood/orders/repository/OrderRepository.java
@@ -1,7 +1,18 @@
 package com.goodfood.orders.repository;
 
 import com.goodfood.orders.model.Order;
+import com.goodfood.orders.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query(value = "SELECT o from Order o LEFT JOIN FETCH o.itens where o.id = :id")
+    Order searchForIdWithItems(Long id);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Order p set o.status = :status where o = :order")
+    void updateStatus(Status status, Order order);
 }
