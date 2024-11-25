c1569a4b58f2f0f9b49a497d377c77b359c724dd
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-11-25 16:40:22 -0300

    feat: add Status enum to represent order states                                                                                                                                                                                                               - Defined the Status enum for Order entity. - Included the following states:   DONE, CANCELLED, PAID, UNAUTHORIZED, CONFIRMED, READY, OUT_FOR_DELIVERY, DELIVERED. - Linked the enum to the Order entity using @Enumerated(EnumType.STRING).

---
+++ src/main/java/com/goodfood/orders/model/Status.java
@@ -0,0 +1,14 @@
+package com.goodfood.orders.model;
+
+public enum Status {
+    DONE,
+    CANCELLED,
+    PAID,
+    UNAUTHORIZED,
+    CONFIRMED,
+    READY,
+    OUT_FOR_DELIVERY,
+    DELIVERED;
+
+
+}
