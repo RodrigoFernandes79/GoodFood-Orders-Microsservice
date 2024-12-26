628f14d0bb728caac12e809eae9c43c007a91189
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2024-12-26 15:38:14 -0300

    feat: consume the JSON data and print it in the application log

--- src/main/java/com/goodfood/orders/amqp/PaymentListener.java
+++ src/main/java/com/goodfood/orders/amqp/PaymentListener.java
@@ -1,14 +1,25 @@
 package com.goodfood.orders.amqp;
 
-import org.springframework.amqp.core.Message;
+import com.goodfood.orders.dto.PaymentDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentListener {
     @RabbitListener(queues = "payment.completed")
-    public void receiveMessage(Message message) {
-        System.out.println("message received " + message.toString());
+    public void receiveMessage(@Payload PaymentDto paymentDto) {
+        System.out.println("""
+                Payment data: %s,
+                Customer name: %s,
+                Order number: %s,
+                Value: R$ %s,
+                Payment Status: %s
+                """.formatted(paymentDto.getId(),
+                paymentDto.getName(),
+                paymentDto.getOrderId(),
+                paymentDto.getValue(),
+                paymentDto.getStatus()));
     }
 
 }
